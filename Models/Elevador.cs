73c3684c2f313feb016f3af465ed3cd2c7e62728
jcarrio <julio.carrio@gmail.com>
2022-01-04 22:28:44 -0300

    Commit inicial

---
+++ Models/Elevador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador_Documentacao.Models
+{
+    public class Elevador
+    {
+        private int AndarAtual { get; set; }
+        private int TotalAndares { get; set; }
+        private int CapacidadeElevador { get; set; }
+        private int PessoasElevador { get; set; }
+    }
+}
